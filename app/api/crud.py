7e957fc72a7f60bd80307008845c96db53e976b5
Slavina Stefanova <sstefanova@wikimedia.org>
2023-05-09 09:43:50 +0200

    add crud helpers

---
+++ app/api/crud.py
@@ -0,0 +1,36 @@
+from app.models.pydantic import SummaryPayloadSchema
+from app.models.tortoise import TextSummary
+from app.summarizer import generate_summary
+
+
+async def get(id: int) -> dict | None:
+    summary = await TextSummary.filter(id=id).first().values()
+    if summary:
+        return summary
+    return None
+
+
+async def get_all() -> list:
+    summaries = await TextSummary.all().values()
+    return summaries
+
+
+async def post(payload: SummaryPayloadSchema) -> int:
+    summary = TextSummary(url=payload.url, summary="")
+    await summary.save()
+    return summary.id
+
+
+async def delete(id: int) -> int:
+    summary = await TextSummary.filter(id=id).first().delete()
+    return summary
+
+
+async def put(id: int, payload: SummaryPayloadSchema) -> dict | None:
+    summary = await TextSummary.filter(id=id).update(
+        url=payload.url, summary=payload.summary
+    )
+    if summary:
+        updated_summary = await TextSummary.filter(id=id).first().values()
+        return updated_summary
+    return None
