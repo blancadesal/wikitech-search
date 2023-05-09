caa2f210cdf5f6255e1d6c3fcf6024ea78c942ee
Slavina Stefanova <sstefanova@wikimedia.org>
2023-05-09 09:42:07 +0200

    add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,18 +2,53 @@ import os
 
 import pytest
 from starlette.testclient import TestClient
+from tortoise.contrib.fastapi import register_tortoise
 
+from app.api import summaries
+from app.config import Settings, get_settings
 from app.main import create_application
-from app.config import get_settings, Settings
 
 
 def get_settings_override():
     return Settings(testing=1, database_url=os.environ.get("DATABASE_TEST_URL"))
 
 
+def create_test_client(app):
+    app.dependency_overrides[get_settings] = get_settings_override
+    return TestClient(app)
+
+
+def register_tortoise_for_test(app):
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_TEST_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+
+
 @pytest.fixture(scope="module")
 def test_app():
     app = create_application()
-    app.dependency_overrides[get_settings] = get_settings_override
-    with TestClient(app) as test_client:
-        yield test_client
+    test_client = create_test_client(app)
+    with test_client as test_client_instance:
+        yield test_client_instance
+
+
+@pytest.fixture(scope="module")
+def test_app_with_db():
+    app = create_application()
+    register_tortoise_for_test(app)
+    test_client = create_test_client(app)
+    with test_client as test_client_instance:
+        yield test_client_instance
+
+
+@pytest.fixture
+def mock_generate_summary(monkeypatch):
+    def generate_summary(id, url):
+        return None
+
+    monkeypatch.setattr(summaries, "generate_summary", generate_summary)
+    return generate_summary
