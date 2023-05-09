ffe9d76bf345fad04f0c31155cf17579d228dd4a
Slavina Stefanova <sstefanova@wikimedia.org>
2023-05-09 21:41:49 +0200

    back to nltk

--- app/api/crud.py
+++ app/api/crud.py
@@ -1,6 +1,5 @@
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
-from app.summarizer import generate_summary
 
 
 async def get(id: int) -> dict | None:
--- app/summarizer.py
+++ app/summarizer.py
@@ -1,21 +1,21 @@
+import nltk
 from newspaper import Article
-from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 from app.models.tortoise import TextSummary
+
 
 async def generate_summary(summary_id: int, url: str) -> None:
     article = Article(url)
     article.download()
     article.parse()
-    text = article.text
 
-    # Load the model and tokenizer
-    model = T5ForConditionalGeneration.from_pretrained("t5-small")
-    tokenizer = T5Tokenizer.from_pretrained("t5-small")
+    try:
+        nltk.data.find("tokenizers/punkt")
+    except LookupError:
+        nltk.download("punkt")
+    finally:
+        article.nlp()
 
-    # Generate the summary
-    inputs = tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=512, truncation=True)
-    summary_ids = model.generate(inputs, max_length=150, num_return_sequences=1, length_penalty=2.0)
-    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    summary = article.summary
 
     await TextSummary.filter(id=summary_id).update(summary=summary)
