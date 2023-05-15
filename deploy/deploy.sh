7bda2f24e6d09b640f813316c74f2f34270b24e0
Slavina Stefanova <sstefanova@wikimedia.org>
2023-05-15 11:15:35 +0200

    set correct paths

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -13,9 +13,9 @@ docker-compose up -d
 
 
 if [ ! -e /etc/nginx/sites-available/wikitech-search.wmcloud.org ] || \
-   ! cmp -s ./nginx.conf /etc/nginx/sites-available/wikitech-search.wmcloud.org
+   ! cmp -s ./deploy/nginx.conf /etc/nginx/sites-available/wikitech-search.wmcloud.org
 then
-    cp ./nginx.conf /etc/nginx/sites-available/wikitech-search.wmcloud.org
+    cp ./deploy/nginx.conf /etc/nginx/sites-available/wikitech-search.wmcloud.org
 
     if [ ! -e /etc/nginx/sites-enabled/wikitech-search.wmcloud.org ]; then
         sudo ln -s /etc/nginx/sites-available/wikitech-search.wmcloud.org /etc/nginx/sites-enabled/
